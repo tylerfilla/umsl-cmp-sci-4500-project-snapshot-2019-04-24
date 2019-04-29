a16c87bbaf95477ab0da1dea0ef0f21fa96bda25
Tyler Filla <tylerfilla@hotmail.com>
2019-04-29 00:12:22 -0500

    Add demonstrative code for tracker

--- cozmonaut/component/client/operation/interact/__init__.py
+++ cozmonaut/component/client/operation/interact/__init__.py
@@ -165,8 +165,147 @@ class OperationInteract(AbstractClientOperation):
         self._face_tracker_a.start()
         self._face_tracker_b.start()
 
+        # TODO: This is where we should read the database into the trackers
+
+        # FIXME: Remove this later; this is Tyler's face encoding
+        tyler_face = (
+            -0.103433,
+            0.0713784,
+            0.0813356,
+            -0.0747395,
+            -0.157589,
+            -0.0386992,
+            -0.0319699,
+            -0.00274016,
+            0.0867231,
+            -0.0220311,
+            0.242471,
+            0.0148122,
+            -0.252416,
+            -0.0551133,
+            -0.0037139,
+            0.0990293,
+            -0.113765,
+            -0.0226992,
+            -0.0938466,
+            -0.0400318,
+            0.126524,
+            0.102942,
+            0.0550079,
+            0.0616467,
+            -0.145211,
+            -0.260875,
+            -0.105383,
+            -0.0524487,
+            0.00731247,
+            -0.135143,
+            0.0509941,
+            0.124918,
+            -0.109638,
+            -0.0350157,
+            0.0340424,
+            0.0950269,
+            -0.0593138,
+            -0.0289018,
+            0.215726,
+            -0.0228096,
+            -0.149361,
+            0.0423131,
+            0.0110523,
+            0.264083,
+            0.194999,
+            0.0382402,
+            0.0235397,
+            -0.0508239,
+            0.100998,
+            -0.320135,
+            0.0635357,
+            0.134587,
+            0.0839489,
+            0.050831,
+            0.0836643,
+            -0.125788,
+            0.0253968,
+            0.212677,
+            -0.222989,
+            0.0768562,
+            -0.0297501,
+            -0.215015,
+            -0.0410392,
+            -0.110664,
+            0.166501,
+            0.0996042,
+            -0.129823,
+            -0.148502,
+            0.147683,
+            -0.152009,
+            -0.145286,
+            0.145061,
+            -0.140681,
+            -0.147379,
+            -0.37368,
+            0.0436715,
+            0.353895,
+            0.153631,
+            -0.225468,
+            0.0191243,
+            -0.01694,
+            0.0200662,
+            0.0228013,
+            0.0611707,
+            -0.0946287,
+            -0.0709029,
+            -0.121012,
+            0.0488099,
+            0.17418,
+            -0.0588228,
+            -0.0645145,
+            0.26763,
+            0.092387,
+            0.115437,
+            0.0444944,
+            0.0116651,
+            -0.00945554,
+            -0.0874052,
+            -0.132031,
+            0.0409098,
+            0.0522451,
+            -0.105967,
+            -0.020343,
+            0.127948,
+            -0.15351,
+            0.168118,
+            -0.0352881,
+            -0.045533,
+            -0.0601219,
+            -0.0499158,
+            -0.139128,
+            0.0365747,
+            0.188973,
+            -0.290735,
+            0.218931,
+            0.203897,
+            0.0409592,
+            0.125365,
+            0.0873372,
+            0.0437877,
+            -0.0335225,
+            -0.054352,
+            -0.145829,
+            -0.065083,
+            0.144216,
+            -0.0487921,
+            0.0604078,
+            0.0337079
+        )
+
+        # FIXME: Remove this
+        self._face_tracker_a.add_identity(42, tyler_face)
+
         # Run the loop on this thread until it stops itself
         loop.run_forever()
+
+        # TODO: This is where we should save from the trackers into the database
 
         # Stop the face trackers
         self._face_tracker_a.stop()
@@ -186,14 +325,8 @@ class OperationInteract(AbstractClientOperation):
                 # We're stopping now
                 self._stopping = True
 
-                while True:
-                    # TODO: We need to wait for both Cozmos to return to their chargers
-                    # TODO: Either add another global boolean or upgrade self._swap to a three-state int (where zero means none active, etc.)
-                    print(
-                        'Drive to charger not implemented yet (_watchdog in cozmonaut/component/client/operation/__init__.py ...)')
-
-                    # Yield control to other coroutines
-                    await asyncio.sleep(0)
+                # TODO: We need to drive both Cozmos back to their chargers, as the Ctrl+C or equivalent happened
+                print('DRIVING TO CHARGER NOT IMPLEMENTED YET')
 
                 # Politely ask the loop to stop
                 loop = asyncio.get_event_loop()
@@ -220,6 +353,8 @@ class OperationInteract(AbstractClientOperation):
 
         # Loop for Cozmo A
         while not self._stopping:
+            # TODO: Swap back and forth between active and idle
+
             # Yield control to other coroutines
             await asyncio.sleep(0)
 
@@ -259,6 +394,8 @@ class OperationInteract(AbstractClientOperation):
 
         # Loop for Cozmo B
         while not self._stopping:
+            # TODO: Swap back and forth between active and idle (but opposite that of Cozmo A)
+
             # Yield control to other coroutines
             await asyncio.sleep(0)
 
@@ -295,7 +432,7 @@ class OperationInteract(AbstractClientOperation):
         """
 
         while not self._stopping:
-            # TODO: This is code that needs to run while on the charger
+            # TODO: Is there any code for when we're on the charger? That would go here
 
             # Yield control to other coroutines
             await asyncio.sleep(0)
@@ -326,7 +463,8 @@ class OperationInteract(AbstractClientOperation):
             # If battery potential is below the recommended "low" level
             if robot.battery_voltage < 3.5:
                 # TODO: Drive the robot back to charge and swap the next one in
-                print('THE BATTERY IS LOW')
+                print('DRIVING TO CHARGER NOT YET IMPLEMENTED')
+                print('SWAPPING THE COZMOS NOT YET IMPLEMENTED')
                 break
 
             # Yield control to other coroutines
@@ -338,14 +476,37 @@ class OperationInteract(AbstractClientOperation):
 
         This is responsible for watching for faces
 
-        :param robot:
+        :param robot: The robot instance
         """
 
         # Enable color imaging on this robot's camera
         robot.camera.color_image_enabled = True
         robot.camera.image_stream_enabled = True
 
+        # Pick the face tracker for this robot
+        # Cozmo A gets tracker A and Cozmo B gets tracker B
+        ft = None
+        if robot == self._robot_a:
+            ft = self._face_tracker_a
+        elif robot == self._robot_b:
+            ft = self._face_tracker_b
+
         while not self._stopping:
+            # Wait for the next face tracked by the tracker
+            track = await asyncio.wrap_future(ft.next_track())
+
+            # TODO: Make Cozmo look at the face for social cue
+            #   Hopefully we don't lose the track b/c motion blur, but I think I know a hack if we do
+
+            await asyncio.sleep(0.25)  # TODO: Let motion blur settle down? Am I overestimating motion blur?
+
+            # Request to recognize the face
+            rec = await asyncio.wrap_future(ft.recognize(track.index))
+
+            # TODO: Greet the face if rec.fid is not negative one
+            #  If rec.fid is negative one, then meet the new person and store a Base64 copy of rec.ident to the DB
+            #  Don't forget to then add it to the trackers with tracker_a.add_identity and tracker_b.add_identity
+
             # Yield control to other coroutines
             await asyncio.sleep(0)
 
